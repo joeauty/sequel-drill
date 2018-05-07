c038838bcfa307abf830f5defa68213e1d4257f5
Carlos Derich <d.erich@hotmail.com>
2018-05-07 14:15:15 -0400

    add option for http read_timeout

--- spec/adapters/drill_spec.rb
+++ spec/adapters/drill_spec.rb
@@ -8,28 +8,45 @@ unless defined?(DRILL)
   DRILL_DB = Sequel.connect(ENV['DRILL_URL']||DRILL_URL)
 end
 
+describe '#connect' do
+
+  it 'should set read_timeout' do
+    conn = Sequel.connect("#{ENV['DRILL_URL']||DRILL_URL}", read_timeout: 9999)
+    conn.synchronize do |c|
+      expect(c.read_timeout).to eq 9999
+    end
+  end
+
+  it 'should keep default read_timeout when not set' do
+    conn = Sequel.connect("#{ENV['DRILL_URL']||DRILL_URL}")
+    conn.synchronize do |c|
+      expect(c.read_timeout).to eq 60
+    end
+  end
+end
+
 describe "import dataset" do
   before do
     @db = DRILL_DB
   end
-  
+
   specify "upload CSV dataset via WebHDFS" do
     conn = Sequel.connect("#{ENV['DRILL_URL']||DRILL_URL}")
     conn.synchronize do |raw_conn|
       webhdfs = WebHDFS::Client.new("#{ENV['HDFS_HOST']}", 50070)
       upload = webhdfs.create("/#{ENV['DRILL_WORKSPACE']}/test.csv", File.open("spec/adapters/test.csv", "r"), :overwrite => true)
-    end 
+    end
   end
-  
+
   specify "create parquet file out of CSV" do
     expect(@db.run("DROP TABLE IF EXISTS dfs.#{ENV['DRILL_WORKSPACE']}.\`test\`")).to eq(nil)
     expect(@db.run("CREATE TABLE dfs.#{ENV['DRILL_WORKSPACE']}.\`test\` AS SELECT CAST(columns[0] AS VARCHAR(65000)) AS \`c1\`, CAST(columns[1] AS FLOAT) AS \`c2\`, CAST(columns[2] AS VARCHAR(65000)) AS \`c3\` FROM dfs.tmp.\`test.csv\`")).to eq(nil)
   end
-  
+
   specify "verify import, cleanup" do
     expect(DRILL_DB[:test].select(:c1).first[:c1]).to eq("foo")
   end
-  
+
   specify "cleanup original CSV" do
     expect(@db.run("DROP TABLE IF EXISTS dfs.#{ENV['DRILL_WORKSPACE']}.\`test.csv\`")).to eq(nil)
   end
@@ -39,7 +56,7 @@ describe "A drill dataset" do
   before do
     @d = DRILL_DB[:test]
   end
-  
+
   specify "Drill workaround: override != operators with <>" do
     expect(@d.where(c1:10).invert.sql).to eq( \
     'SELECT * FROM "test" WHERE "c1" <> 10'
@@ -49,7 +66,7 @@ describe "A drill dataset" do
   specify "Drill workaround: aggregate methods column display names should be escaped with backticks using Drill workspace" do
     # generated query should be "SELECT count(name) AS `count` FROM dfs.tmp.`test` LIMIT 1"
     expect(@d.count(:c1)).to eq("3")
-    
+
     # test the other methods just for good measure
     expect(@d.max(:c2)).to eq("66.0")
     expect(@d.min(:c2)).to eq("11.0")
@@ -65,7 +82,7 @@ describe "A drill dataset" do
     expect(@d.select(Sequel.lit('COUNT(*)')).sql).to eq( \
       'SELECT COUNT(*) FROM "test"'
     )
-    
+
     expect(@d.select(:max.sql_function(:value)).sql).to eq( \
       'SELECT max("value") FROM "test"'
     )
@@ -81,7 +98,7 @@ describe "A drill dataset" do
     expect(@d.order(:name.desc).sql).to eq( \
       'SELECT * FROM "test" ORDER BY "name" DESC'
     )
-    
+
     expect(@d.select(Sequel.lit('test.name AS item_name')).sql).to eq( \
       'SELECT test.name AS item_name FROM "test"'
     )
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,8 @@ rescue LoadError
   $stderr.puts "Please create a file spec/spec_config.rb with a database connection string."
   raise
 end
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run focus: true
+end
